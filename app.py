6b38a8f7e2d4723c9667d9ea4e50db6207e7e20b
yafagroup131-cmd <yafagroup131@gmail.com>
2026-05-03 14:59:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,15 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 # ===== UI =====
-st.title("🤖 Retriva RAG")
-
-st.image("assets/logo.png", width=200)
-
-
+st.markdown(
+    """
+    <div style="text-align: center;">
+        <img src="assets/logo.png" width="200">
+        <h1>🤖 Retriva RAG</h1>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 st.markdown("""
 <style>
 
