6cf72240c246c2a895c1025a2ea51e77acf95870
yafagroup131-cmd <yafagroup131@gmail.com>
2026-05-06 00:30:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,7 @@ retriever = vectorstore.as_retriever()
 import os
 
 llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
+    model="gemini-1.5-flash-latest",
     temperature=0.2,
     google_api_key=st.secrets["GOOGLE_API_KEY"]
 )
